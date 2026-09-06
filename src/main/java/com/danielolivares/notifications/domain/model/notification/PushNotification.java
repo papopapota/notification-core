f5a6bb82e0be95b73cdeba5a786fbadf89d8bc84
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2026-09-06 17:14:48 -0500

    refactor(norification): se mejoro y se agrego test para notification y sus tipos

--- src/main/java/com/danielolivares/notifications/domain/model/notification/PushNotification.java
+++ src/main/java/com/danielolivares/notifications/domain/model/notification/PushNotification.java
@@ -1,13 +1,23 @@
 package com.danielolivares.notifications.domain.model.notification;
 
 import com.danielolivares.notifications.domain.model.EnumNotificationChannel;
-import com.danielolivares.notifications.domain.model.Recipient;
+import com.danielolivares.notifications.domain.model.recipient.Recipient;
+
+import java.util.Objects;
+import java.util.UUID;
 
 public record PushNotification(
         String id,
         Recipient recipient,
         String content
 ) implements Notification {
+    public PushNotification{
+        id = (id == null || id.isBlank()) ? UUID.randomUUID().toString() : id;
+        Objects.requireNonNull(recipient, "Slack channel cannot be null");
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("Content cannot be null or empty");
+        }
+    }
     @Override
     public EnumNotificationChannel channel() {
         return EnumNotificationChannel.PUSH;
